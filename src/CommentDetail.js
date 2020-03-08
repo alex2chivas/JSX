64de2cc46b5e78c8ff05fdcf613d9c9b6d6c42d3
Alexis Flores <alex2chivas@hotmail.com>
2020-03-07 22:21:45 -0800

    added more props for CommentDetail files

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = (props) => {
-  const date = new Date();
   return (
     <div className='comment'>
       <a href='/' className='avatar'>
-        <img alt='avatar' src={faker.image.avatar()} />
+        <img alt='avatar' src={props.src} />
       </a>
       <div className='content'>
         <a href='/' className='author'>
           {props.author}
         </a>
         <div className='metadata'>
-          <span className='date'>{date.toUTCString()}</span>
+          <span className='date'>{props.timeAgo}</span>
         </div>
-        <div className='text'> {faker.lorem.text()}</div>
+        <div className='text'> {props.commentsText}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
 
-const App = (props) => {
+const App = () => {
+  const date = new Date();
   return (
     <div className='ui container comments'>
-      <CommentDetail author={'Alexis'} />
-      <CommentDetail author={'James'} />
-      <CommentDetail author={'Frank'} />
-      <CommentDetail author={'Freddie'} />
-      <CommentDetail author={'David'} />
+      <CommentDetail
+        author={'Alexis'}
+        timeAgo={date.toLocaleDateString()}
+        src={faker.image.avatar()}
+        commentsText={faker.lorem.text()}
+      />
+      <CommentDetail
+        author={'James'}
+        timeAgo={date.toISOString()}
+        src={faker.image.avatar()}
+        commentsText={faker.lorem.text()}
+      />
+      <CommentDetail
+        author={'Frank'}
+        timeAgo={date.toUTCString()}
+        src={faker.image.avatar()}
+        commentsText={faker.lorem.text()}
+      />
+      <CommentDetail
+        author={'Freddie'}
+        timeAgo={date.toTimeString()}
+        src={faker.image.avatar()}
+        commentsText={faker.lorem.text()}
+      />
+      <CommentDetail
+        author={'David'}
+        timeAgo='Today at 1:45pm'
+        src={faker.image.avatar()}
+        commentsText={faker.lorem.text()}
+      />
     </div>
   );
 };
