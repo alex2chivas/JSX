89a4050113cb3809f40021faf37226807560b7d4
Alexis Flores <alex2chivas@hotmail.com>
2020-03-08 18:34:14 -0700

    final push

--- src/index.js
+++ src/index.js
@@ -7,40 +7,102 @@ import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   const date = new Date();
-  return (
-    <div className='ui container comments'>
-      <CommentDetail
-        author={'Alexis'}
-        timeAgo={date.toLocaleDateString()}
-        src={faker.image.avatar()}
-        commentsText={faker.lorem.text()}
-      />
-      <CommentDetail
-        author={'James'}
-        timeAgo={date.toISOString()}
-        src={faker.image.avatar()}
-        commentsText={faker.lorem.text()}
-      />
-      <CommentDetail
-        author={'Frank'}
-        timeAgo={date.toUTCString()}
-        src={faker.image.avatar()}
-        commentsText={faker.lorem.text()}
-      />
-      <CommentDetail
-        author={'Freddie'}
-        timeAgo={date.toTimeString()}
-        src={faker.image.avatar()}
-        commentsText={faker.lorem.text()}
-      />
-      <CommentDetail
-        author={'David'}
-        timeAgo='Today at 1:45pm'
-        src={faker.image.avatar()}
-        commentsText={faker.lorem.text()}
-      />
-    </div>
+  return ( <
+    div className = 'ui container comments' >
+
+    <
+    ApprovalCard >
+    <
+    div >
+    <
+    h4 > Warning! < /h4>
+    Are you sure you want to do this ?
+      <
+      /div> <
+      /ApprovalCard>
+
+      <
+      ApprovalCard >
+      <
+      CommentDetail
+    author = {
+      'Alexis'
+    }
+    timeAgo = {
+      date.toLocaleDateString()
+    }
+    src = {
+      faker.image.avatar()
+    }
+    commentsText = {
+      faker.lorem.text()
+    }
+    /> <
+    /ApprovalCard> <
+    ApprovalCard >
+    <
+    CommentDetail author = {
+      'James'
+    }
+    timeAgo = {
+      date.toISOString()
+    }
+    src = {
+      faker.image.avatar()
+    }
+    commentsText = {
+      faker.lorem.text()
+    }
+    /> <
+    /ApprovalCard> <
+    ApprovalCard >
+    <
+    CommentDetail author = {
+      'Frank'
+    }
+    timeAgo = {
+      date.toUTCString()
+    }
+    src = {
+      faker.image.avatar()
+    }
+    commentsText = {
+      faker.lorem.text()
+    }
+    /> <
+    /ApprovalCard> <
+    ApprovalCard >
+    <
+    CommentDetail author = {
+      'Freddie'
+    }
+    timeAgo = {
+      date.toTimeString()
+    }
+    src = {
+      faker.image.avatar()
+    }
+    commentsText = {
+      faker.lorem.text()
+    }
+    /> <
+    /ApprovalCard> <
+    ApprovalCard >
+    <
+    CommentDetail author = {
+      'David'
+    }
+    timeAgo = 'Today at 1:45pm'
+    src = {
+      faker.image.avatar()
+    }
+    commentsText = {
+      faker.lorem.text()
+    }
+    /> <
+    /ApprovalCard> <
+    /div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render( < App / > , document.querySelector( '#root' ) );
